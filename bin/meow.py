29c48bb7bfea76c7ebf7a2a7a879ee9b4d44725a
Michael Crilly <mcrilly@skyscapecloud.com>
2014-01-20 17:58:32 +0000

    Attempting a restructure. Failing.

---
+++ bin/meow.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+libdir = os.path.join(os.path.dirname(sys.path[0]), '../lib/')
+sys.path.append(libdir)
+
+# import rita
+
+if __name__ == '__main__':
+	# r = Rita()
+	# print r 
+	print sys.path
